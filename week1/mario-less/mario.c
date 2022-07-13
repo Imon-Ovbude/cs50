6b7c4adf7b714a0a2d34cb591602169faf02f11c
Imon-Ovbude <imonhimio@gmail.com>
2022-07-12 18:06:23 -0700

    created for loop to display block number of i times on each row

--- week1/mario-less/mario.c
+++ week1/mario-less/mario.c
@@ -7,9 +7,17 @@ int main(void)
     int height;
 
     do {
+        // prompt user for a number between 1 and 8
         height = get_int("Enter a number between 1 and 8\n ");
     } while (height < 1 || height > 8 );
-    
 
-    printf("Stored %i\n", height);
-}
+    // loop through height variable
+    for (int i = 1; i <= height; i++) {
+        // print block number of i
+        for (int j = 1; j <= i; j++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+    
+} 
