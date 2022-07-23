7fa8063b42da70a27db181cbf991a28f4b744f64
Imon-Ovbude <imonhimio@gmail.com>
2022-07-23 16:27:32 -0700

    use strlen from stering library

---
+++ practice/length.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// int string_length(string s);
+
+int main(void) {
+    // string name = get_string("Name: ");
+
+    // int length = string_length(name);
+
+    // printf("%i\n", length);
+
+    string name = get_string("Name: ");
+
+    int length = strlen(name);
+
+    printf("%i\n", length);
+}
+
+// ---Helper Function---
+// int string_length(string s) {
+//     int i = 0;
+//     while (s[i] != '\0') {
+//         // \0 means nul
+//         i++;
+//     }
+
+//    return i;
+// }
