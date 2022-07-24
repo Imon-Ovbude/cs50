494437ad2340cbb613d07444cca848099f34cd9d
Imon-Ovbude <imonhimio@gmail.com>
2022-07-23 17:17:14 -0700

    command line arguments

---
+++ practice/argv.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Command Line Arguments
+int main(int argc, string argv[]) {
+    if (argc == 2) {
+    printf("hello, %s\n", argv[1]);
+    } else {
+        printf("Hello World\n");
+    }
+}
